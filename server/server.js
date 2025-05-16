5fa22d7c3d38eb8317539ace2b7722fac370edaa
Jayesh Berani <109126397+jayeshberanii@users.noreply.github.com>
2025-05-16 18:46:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,10 +11,12 @@ app.use(express.urlencoded({extended:true}))
 app.use('/api/users',require('./routes/userRoute'))
 app.use('/api/todos',require('./routes/todoRoute'))
 app.post('/webhook', (req,res)=>{
-    console.log("::: Webhook called with :::", req);
+    console.log("::: Webhook called with :::", req.body);
+     res.status(200).json("::: Webhook called with :::", req.body)
 })
 app.get('/test', (req,res)=>{
-    console.log("::: test url working :::", req);
+    console.log("::: test url working :::", req.body);
+    res.status(200).json("::: test url working :::", req.body)
 })
 
 app.listen(process.env.PORT,()=>{
